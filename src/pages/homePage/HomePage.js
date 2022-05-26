ba48a8b08c79c036d9816f00a17501fa14109298
Asuraking1n <nishant99tiwari@gmail.com>
2022-05-26 12:09:35 +0530

    fix homepage

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -4,15 +4,17 @@ import circlebg from "../../images/circle-bg.png";
 import Linkedin from "../../images/linkedin.png";
 import Instagram from "../../images/instagram.png";
 import Github from "../../images/github.png";
+import {useNavigate} from 'react-router-dom'
 const HomePage = () => {
+    const navigate = useNavigate()
     return (
         <>
             <div className="homepage-cont-sec">
                 <div className="front-text">
                     <span>Fashion and Beauty</span> <br />
-                    Only Amayra's Gallery
+                    Amayra's Video Gallery
                     <br />
-                    <span>.....watch reels</span>
+                    <span onClick={()=>navigate('/vedios')}>.....watch reels</span>
                 </div>
                 <div className="compass-sec">
                     <img src={circlebg} alt="c" />
